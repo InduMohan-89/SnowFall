4a497bd6e435493fd6212337d21abf0b9eec7a9f
InduMohan-89 <73943698+InduMohan-89@users.noreply.github.com>
2022-01-18 17:47:41 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var sn1,sn2,bg,sn4,engine,world,snowObj;
+
+function preload() {
+bg = loadImage("snow3.jpg");
+
+
+
+}
+function setup() {
+  createCanvas(800,600);
+  engine = Engine.create();
+  world = engine.world;
+snowObj = new Snow(200,10);
+ 
+
+  
+}
+
+function draw() {
+  background(bg); 
+  Engine.update(engine);
+
+  snowObj.display();
+
+  
+  
+}
